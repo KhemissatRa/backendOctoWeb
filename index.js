dbb745f0f51b7a6f7096a0fa4d3b92689d4ded46
khemissat <khemissat.ra@gmail.com>
2025-11-29 18:43:58 +0100

    add error checking

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // optional, for form data
 
 const port = process.env.PORT || 5000;
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
